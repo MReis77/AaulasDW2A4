92ae48514755dcbc22f7b4bee9661bf455655484
Marco <marcojunior.reis@hotmail.com>
2021-06-12 13:52:23 -0300

    Exemplode ECMA7 e versões anteriores

---
+++ Aula 3/Exemplo ECMA7.js
@@ -0,0 +1,19 @@
+/*A partir do ECMA7 pode iniciar as propriedades 
+da classe fora do construtor, isso facilita o acesso
+sem precisar referenciar*/
+
+
+class Animal {
+
+    constructor() {
+
+        this.name = "Lion"
+
+    }
+
+    age = 0;
+
+}
+
+
+ 
---
+++ Aula 3/Exemplo EMCA 5.js
@@ -0,0 +1,14 @@
+/*Já nas versões anteriores as propriedades 
+tem q ser declaradas dentro do construtor*/
+
+
+class Animal {
+
+    constructor() {
+
+        this.age = 0;
+        this.name = "Lion";
+
+    }
+
+    }
